22c8ff2fe96c01ef2fa2e3ff9ca3ac4a347984b2
Andoni <andonisalcedo@gmail.com>
2020-12-01 12:46:25 +0100

    beta 2.1

--- backend/install.js
+++ backend/install.js
@@ -1,4 +1,21 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const syncro = require('./services/synchro')
 
-syncro.CommunityInit()
-syncro.RegionInit()
+
+dotenv.config()
+
+mongoose.connect(process.env.DB || 'mongodb://localhost:27017/iscovid',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', () => {
+    console.log('DB connected')
+    syncro.CommunityInit()
+    syncro.RegionInit()
+})
+
+
+
